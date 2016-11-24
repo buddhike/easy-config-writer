8001313aa60b0007a51637b9d9f2cf15e6741e18
Buddhike de Silva <buddhike.desilva@williamhill.com.au>
2016-11-24 12:47:29 +1100

    Basic working pipeline.

--- src/index.js
+++ src/index.js
@@ -6,36 +6,43 @@ import readInputFile from './readInputFile';
 import parseProperties from './parseProperties';
 import writeOutput from './writeOutput';
 
-const eithert = either => either.fold(Task.rejected, Task.of);
-const propt = id => R.pipe(R.prop(id), Task.of);
-
+const eitherT = either => either.fold(Task.rejected, Task.of);
+const propT = id => R.pipe(R.prop(id), Task.of);
 const waitAll = R.traverse(Task.of, R.identity);
 
-const extractConfigs = R.curry(function(namespaces, properties) {
+const extract = R.curry(function(namespaces, properties) {
   const mapper = namespace => ({ namespace, content: properties[namespace] });
   return R.map(mapper, namespaces);
 });
 
-const write = targets => {
-  const outputFilename = parsed.get().output;
+const buildFiles = R.curry((outputFilename, extracts) => {
+  console.log(outputFilename);
+  return R.map(e => ({
+    file: R.replace('$namespace$', e.namespace, outputFilename),
+    content: e.content
+  }), extracts);
+});
 
-  const files = R.map(t => ({
-    file: R.replace(outputFilename, t.namespace),
-    content: t.content
-  }));
-
-  const f = R.pipe(files, writeOutput, waitAll);
-  return f(targets);
+const extractT = eitherArgs => {
+  return eitherArgs.fold(Task.rejected,
+    a => R.pipe(extract(a.namespaces), Task.of));
 };
 
-const parsed = parseArgs(argv);
-const extractConfigsT = R.pipe(R.prop('namespaces'), extractConfigs, Task.of);
+const buildFilesT = eitherArgs => {
+  return eitherArgs.fold(Task.rejected,
+    a => R.pipe(buildFiles(a.outFile), Task.of));
+};
 
-const pipeline = R.pipe(eithert, R.pipeK(
-  propt('input'),
+/* Setup the pipeline and execute it to observe the side effects */
+const parsed = parseArgs(argv);
+
+const pipeline = R.pipe(eitherT, R.pipeK(
+  propT('input'),
   readInputFile,
   R.pipe(buffer => buffer.toString('utf8'), Task.of),
-  R.pipe(parseProperties, eithert),
-  extractConfigsT(parsed)));
+  R.pipe(parseProperties, eitherT),
+  extractT(parsed),
+  buildFilesT(parsed),
+  R.pipe(R.map(writeOutput), waitAll)));
 
 pipeline(parsed).fork(console.log, console.log);
--- src/writeOutput.js
+++ src/writeOutput.js
@@ -3,11 +3,17 @@ import R from 'ramda';
 import Task from 'data.task';
 
 /**
- * (String, String) -> Task(String, String)
+ * {String, String} -> Task(String, String)
  */
 function writeOutput(output) {
   return new Task((reject, resolve) => {
-    fs.writeFile(output.file, 'utf8', output.content);
+    fs.writeFile(output.file, JSON.stringify(output.content), 'utf8', err => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(output);
+      }
+    });
   });
 }
 
